fe6dc3877375f114f4659439dca976d79a663f2b
Andrei Silchankau <andrei.silchankau@thomsonreuters.com>
2017-09-27 16:08:53 +0300

    login service unit testing

--- src/app/login/login.service.spec.ts
+++ src/app/login/login.service.spec.ts
@@ -1,15 +1,150 @@
+import { Observable } from 'rxjs/Rx';
 import { TestBed, inject } from '@angular/core/testing';
-
 import { LoginService } from './login.service';
+import { Http, HttpModule, XHRBackend, Response, ResponseOptions } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+import { LocalStorageService } from 'ngx-webstorage';
+import { CurrentUserKey } from './login.interfaces';
 
-describe('LoginService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [LoginService]
+function testSequence<Result>(input: Observable<Result>, result: Result[]) {
+    let step = 0;
+    input.subscribe(value => {
+        expect(value).toEqual(result[step]);
+        step++;
     });
-  });
+}
 
-  it('should be created', inject([LoginService], (service: LoginService) => {
-    expect(service).toBeTruthy();
-  }));
+describe('Login Service', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [ HttpModule ],
+            providers: [
+                LoginService,
+                LocalStorageService,
+                { provide: XHRBackend, useClass: MockBackend }
+            ]
+        });
+    });
+
+    it('should return hot login state', inject([LoginService, XHRBackend], (service, mockbackend) => {
+        const response = {
+            firstName: 'a',
+            lastName: 'b',
+            token: 'c'
+        };
+
+        mockbackend.connections.subscribe(connection => {
+            connection.mockRespond(new Response(new ResponseOptions({
+                body: JSON.stringify(response)
+            })));
+        });
+
+        testSequence(service.login, [
+            {
+                status: 0
+            },
+            {
+                status: 1,
+                value: response
+            }
+        ]);
+
+        service.input.next({});
+    }));
+
+    it('should return completed login state', inject([LoginService, XHRBackend], (service, mockbackend) => {
+
+        const response = {
+            firstName: 'a',
+            lastName: 'b',
+            token: 'c'
+        };
+
+        mockbackend.connections.subscribe(connection => {
+            connection.mockRespond(new Response(new ResponseOptions({
+                body: JSON.stringify(response)
+            })));
+        });
+
+        testSequence(service.loginCompleted, [ response ]);
+        service.input.next({});
+    }));
+
+    it('should return null as initial user state', inject([LoginService, XHRBackend, LocalStorageService],
+        (service, mockbackend, localStorageService) => {
+        localStorageService.clear();
+        service = new LoginService(TestBed.get(Http), TestBed.get(LocalStorageService));
+
+        testSequence(service.user, [ null ]);
+    }));
+
+    it('should return not empty initial user state', inject([LoginService, XHRBackend, LocalStorageService],
+        (service, mockbackend, localStorageService) => {
+        const initialState = {
+            firstName: 'a',
+            lastName: 'b',
+            token: 'c'
+        };
+
+        localStorageService.store(CurrentUserKey, initialState);
+        service = new LoginService(TestBed.get(Http), TestBed.get(LocalStorageService));
+
+        testSequence(service.user, [ initialState ]);
+    }));
+
+    it('should reflect login to the user', inject([LoginService, XHRBackend, LocalStorageService],
+        (service, mockbackend, localStorageService) => {
+        const initialState = {
+            firstName: 'a',
+            lastName: 'b',
+            token: 'c'
+        };
+
+        const response = {
+            firstName: 'c',
+            lastName: 'e',
+            token: 'd'
+        };
+
+        localStorageService.store(CurrentUserKey, initialState);
+        service = new LoginService(TestBed.get(Http), TestBed.get(LocalStorageService));
+
+        mockbackend.connections.subscribe(connection => {
+            connection.mockRespond(new Response(new ResponseOptions({
+                body: JSON.stringify(response)
+            })));
+        });
+
+        testSequence(service.user, [ initialState, response ]);
+        service.input.next({});
+    }));
+
+    it('should keep last user state', inject([LoginService, XHRBackend, LocalStorageService],
+        (service, mockbackend, localStorageService) => {
+        const initialState = {
+            firstName: 'a',
+            lastName: 'b',
+            token: 'c'
+        };
+
+        const response = {
+            firstName: 'c',
+            lastName: 'e',
+            token: 'd'
+        };
+
+        localStorageService.store(CurrentUserKey, initialState);
+        service = new LoginService(TestBed.get(Http), TestBed.get(LocalStorageService));
+
+        mockbackend.connections.subscribe(connection => {
+            connection.mockRespond(new Response(new ResponseOptions({
+                body: JSON.stringify(response)
+            })));
+        });
+
+        service.input.next({});
+        testSequence(service.user, [ response ]);
+    }));
+
+
 });
